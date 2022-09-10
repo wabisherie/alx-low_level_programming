fe0ba139c38e6780850f882ddc2397cf69679b02
wabisherie <wabisherie@gmail.com>
2022-09-09 20:01:18 -0700

    9

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,36 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
 /**
- *main - Entry point
- *return = Always 0 (Success)
+ *main - prints the last git of a random number
+ *
+ * Description: main function
+ * return : Always 0 (Success)
  **/
 
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
-	printf("Last digit of %d, n is\n");
-
-	if(n > 5) {
-		printf("and is greater than 5\n");
+	if (last > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
-
-	if(n ==0) {
-		printf("and is 0\n");
+	if (last == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-
-	else if(n < 6 !& 0) {
-		printf("and is less than 6 and not 0\n");
+	else if (last < 6)
+	{
+		printf("Last digit is %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
-
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*main - Prints all single digits with , and space followed by new line
+*
+*Return: returns 0 (Success)
+**/
+
+int main(void)
+
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+
+	{
+
+		putchar(digit);
+
+		if (digit == '9')
+
+			continue;
+
+		putchar(',');
+
+		putchar(' ');
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
