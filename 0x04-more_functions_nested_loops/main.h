8a13a276cbe183bea9b29ba7d628f2a7f6eb67b9
wabisherie <wabisherie@gmail.com>
2022-09-15 18:11:22 -0700

    corrected main file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 /**
 *File - main.h
 *
@@ -7,6 +9,7 @@
 *
 */
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -17,3 +20,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#endif /*MAIN_H*/
