f94eefbc08965ec3f0f4686347cd1d762f765f17
wabisherie <wabisherie@gmail.com>
2022-10-11 12:38:29 -0700

    absolute value is always positve

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,21 @@
+#include "3-function_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+
+	printf("%d, %d\n", i, j);
+
+	return (0);
+}
