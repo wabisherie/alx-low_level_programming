cf8a6b0d7f5ccb65185e1bf061cbe7d05139ed60
wabisherie <wabisherie@gmail.com>
2022-10-11 13:05:53 -0700

    sum macro function

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include "4-sum.h"
+#include <stdio.h>
+
+/**
+ * main - check my code
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+
+	s = SUM(55, 600);
+	printf("%d\n", s);
+
+	return (0);
+}
