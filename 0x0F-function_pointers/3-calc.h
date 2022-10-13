92de7815b7a20535ad1b8314237f4acf203ff923
wabisherie <wabisherie@gmail.com>
2022-10-13 15:38:30 -0700

    this function pointer

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,18 @@
+#ifndef 3_CALC_H
+#define 3_CALC_H
+
+/*struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+    char *op;
+
+    int (*f)(int a, int b);
+
+} op_t;
+
+#endif
